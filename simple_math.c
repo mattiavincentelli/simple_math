0c64f54e373fa495e53cb8072cfe5c60e32a086a
Mattia Vincentelli <mattiavincentelli5@gmail.com>
2026-03-03 12:55:41 +0100

    aggiunti numeri complessi

--- simple_math.c
+++ simple_math.c
@@ -14,3 +14,18 @@ int sum(int a, int b){
 float avg(int a, int b){
     return (float)(sum(a, b))/2;
  }
+
+
+ complex c_sum(complex a, complex b){
+    complex c;
+    c.real = a.real + b.real;
+    c.imaginary = a.imaginary + b.imaginary;
+    return c;
+ }
+
+ complex c_avg(complex a, complex b){
+    complex sum = c_sum(a, b);
+    sum.real /= 2;
+    sum.imaginary /= 2;
+    return sum;
+ }
--- simple_math.h
+++ simple_math.h
@@ -2,11 +2,24 @@
        \brief a toy example of a header
        \author Mattia Vincentelli
 */
+#include <math.h>
+
+
+
 
 #ifndef _SIMPLEMATH_
     #define _SIMPLEMATH_
         
+        typedef struct complex_n{
+            float real;
+            float imaginary;
+        } complex;
+        
         int sum(int a, int b);
 
         float avg(int a, int b);
+
+        complex c_sum(complex a, complex b);
+        
+        complex c_avg(complex a, complex b);
 #endif
